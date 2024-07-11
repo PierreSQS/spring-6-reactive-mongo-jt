560231fe06151fe54db9ce88e4f33bb5f2715beb
PierreSQS <nomo_messina@web.de>
2024-07-11 23:19:21 +0200

    Sec30_Chap299: added Test to save a Beer

---
+++ src/test/java/guru/springframework/reactivemongo/services/BeerServiceImplTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.reactivemongo.services;
+
+import guru.springframework.reactivemongo.domain.Beer;
+import guru.springframework.reactivemongo.mappers.BeerMapper;
+import guru.springframework.reactivemongo.model.BeerDTO;
+import guru.springframework.reactivemongo.repositories.BeerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+class BeerServiceImplTest {
+
+    @Autowired
+    BeerMapper beerMapper;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    BeerDTO beerDTO;
+
+    @BeforeEach
+    void setUp() {
+        beerDTO = beerMapper.beerToBeerDto(getTestBeer());
+    }
+
+    @Test
+    void saveBeer() {
+    }
+
+    public static Beer getTestBeer() {
+        return Beer.builder()
+                .beerName("33 Export")
+                .beerStyle("PILSENER")
+                .price(new BigDecimal("12.45" ))
+                .upc("12345678")
+                .build();
+    }
+}
