20e28ff40a2a410c3abe8316b057d893d5dea022
PierreSQS <nomo_messina@web.de>
2024-07-12 07:03:04 +0200

    Sec30_Chap300: using awaitility

--- src/test/java/guru/springframework/reactivemongo/services/BeerServiceImplTest.java
+++ src/test/java/guru/springframework/reactivemongo/services/BeerServiceImplTest.java
@@ -10,6 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 class BeerServiceImplTest {
@@ -28,14 +31,19 @@ class BeerServiceImplTest {
     }
 
     @Test
-    void saveBeer() throws InterruptedException {
+    void saveBeer() {
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+
         Mono<BeerDTO> beerDTOSavedMono = beerService.saveBeer(Mono.just(beerDTO));
 
         beerDTOSavedMono.subscribe(savedDto -> {
-            System.out.println(savedDto.getId());
+            System.out.println("The saved BeerID: "+savedDto.getId());
+            atomicBoolean.set(true);
         });
 
-        Thread.sleep(1000L);
+        await().untilTrue(atomicBoolean);
+
+
     }
 
     public static Beer getTestBeer() {
